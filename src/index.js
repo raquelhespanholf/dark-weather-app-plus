2853f899cbd6f252ef49c27b80666925fe542c53
Raquel Hespanhol <68480057+raquelhespanholf@users.noreply.github.com>
2024-04-16 09:15:36 -0300

    Inserted V1 of the loop

--- src/index.js
+++ src/index.js
@@ -53,6 +53,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="row">
+    <div class="col-2">
+      <div class="weather-forecast-date">Thu</div>
+
+      <img src="" alt="" width="36" />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">18</span>
+        <span class="weather-forecast-temperature-min">12</span>
+      </div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
